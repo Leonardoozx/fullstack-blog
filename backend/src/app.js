ff317d1ea176fa183668f1ae949aa480412c422b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 01:05:30 -0300

    implements login routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
+const loginRoutes = require('./routes/login.routes');
+
 const app = express();
 
-app.use(cors);
-
-app.get('/', (_req, res) => res.status(200).json({ message: 'oapsoda' }));
+app.use(cors());
 
 app.use(express.json());
+
+app.use('/login', loginRoutes);
 
 module.exports = app;
