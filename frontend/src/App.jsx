d1ccd99a1914bd5a80205ed3cd04cc96a2ee967a
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 18:49:23 -0300

    feat: implements the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Feed from "./pages/Feed"
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
-    <div>
-        {/* sidebar (lado esquerdo) */}
-        {/* componente principal no meio */}
-        {/* contagem de usuários logados (bônus se tiver tempo - lado direito) */}
-        input
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/signUp" element={ <SignUp /> } />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
