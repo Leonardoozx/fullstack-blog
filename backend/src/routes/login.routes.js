84d06408f785680aacbceedfd78cddb98e3bf84b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 02:17:44 -0300

    feat: implements the login middlewares in the login routes

--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const LoginController = require('../controller/login.controller');
+const LoginMiddlewares = require('../middlewares/loginMiddlewares');
 
 const router = express.Router();
 
 const loginController = new LoginController();
+const loginMiddlewares = new LoginMiddlewares();
 
-router.post('/', loginController.login);
-router.post('/signUp', loginController.signUp);
+router.post('/', loginMiddlewares.signUpMiddleware, loginController.login);
+router.post(
+  '/signUp',
+  loginMiddlewares.loginMiddleware,
+  loginController.signUp
+);
 
 module.exports = router;
