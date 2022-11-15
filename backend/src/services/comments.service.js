b90c43767460140fafd115cc8d197abb8fd05f4e
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 01:33:21 -0300

    implements the insertcomment method for comment services

--- backend/src/services/comments.service.js
+++ backend/src/services/comments.service.js
@@ -1,13 +1,27 @@
-const { Comments } = require('../db/models');
+const { Comments, User } = require('../db/models');
 
 class CommentsServices {
   _formatedDate = new Date().toLocaleString().split(', ').join(' ');
   // async function bellow
   getAllComments = () => Comments.findAll();
 
+  _findUserByUserId = (id) => User.findOne({ where: { id } });
+
   postNewComment = async ({ content, userId }) => {
-    const insertedComment = await Comments.create({ content, userId, date: this._formatedDate });
-    return insertedComment;
+    // the next 2 lines is verifying if user exists before trying to insert a comment in DB and breaking the code
+    const user = await this._findUserByUserId(userId);
+    if (!user) return { type: 'ERROR', message: 'User does not exists' };
+
+    const insertedComment = await Comments.create({
+      content,
+      userId,
+      date: this._formatedDate,
+    });
+    return { type: null, message: insertedComment };
+  };
+
+  updateCommentById = async () => {
+
   };
 }
 
