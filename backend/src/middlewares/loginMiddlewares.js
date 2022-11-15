4d618a7730ead5216690fb13b445d679fb61553b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 02:17:10 -0300

    feat: implements login middlewares

---
+++ backend/src/middlewares/loginMiddlewares.js
@@ -0,0 +1,22 @@
+const { User } = require('../db/models');
+
+class LoginMiddlewares {
+  // async function bellow
+  _findUserByEmail = (id) => User.findOne({ where: { id } });
+
+  signUpMiddleware = async ({ body: { email } }, res, next) => {
+    const user = await this._findUserByEmail(email);
+    if (user) return res.status(400).json({ message: 'User already exists' });
+    next();
+  };
+
+  loginMiddleware = async ({ body: { email } }, res, next) => {
+    const user = await this._findUserByEmail(email);
+    if (!user) return res.status(404).json({ message: 'User does not exists' });
+    if (password !== user.password)
+      return res.status(400).json({ message: 'Wrong password' });
+    next();
+  };
+}
+
+module.exports = LoginMiddlewares;
