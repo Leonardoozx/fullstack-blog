4bc1234212956d92b2c708e53bebed76ae473917
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 02:17:33 -0300

    feat: implements the comments middlewares in the comments routes

--- backend/src/routes/comments.routes.js
+++ backend/src/routes/comments.routes.js
@@ -1,14 +1,28 @@
 const express = require('express');
 
 const CommentsController = require('../controller/comments.controller');
+const CommentsMiddlewares = require('../middlewares/commentsMiddlewares');
 
 const router = express.Router();
 
+const commentsMiddlewares = new CommentsMiddlewares();
 const commentsController = new CommentsController();
 
 router.get('/', commentsController.getAllComments);
-router.post('/', commentsController.postNewComment);
-router.put('/:id', commentsController.updateCommentById);
-router.delete('/:id', commentsController.deleteCommentById);
+router.post(
+  '/',
+  commentsMiddlewares.postNewCommentMiddleware,
+  commentsController.postNewComment
+);
+router.put(
+  '/:id',
+  commentsMiddlewares.updateAndDeleteCommentMiddleware,
+  commentsController.updateCommentById
+);
+router.delete(
+  '/:id',
+  commentsMiddlewares.updateAndDeleteCommentMiddleware,
+  commentsController.deleteCommentById
+);
 
 module.exports = router;
