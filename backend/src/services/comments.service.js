f0a0e55b747d1c86a94b69de7d1e722e117772c7
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 02:18:51 -0300

    feat: refactor postNewComment method for the middleware usage
    feat: implements updateCommentById and deleteCommentById methods

--- backend/src/services/comments.service.js
+++ backend/src/services/comments.service.js
@@ -1,27 +1,31 @@
-const { Comments, User } = require('../db/models');
+const { Comments } = require('../db/models');
 
 class CommentsServices {
   _formatedDate = new Date().toLocaleString().split(', ').join(' ');
+
   // async function bellow
   getAllComments = () => Comments.findAll();
 
-  _findUserByUserId = (id) => User.findOne({ where: { id } });
-
   postNewComment = async ({ content, userId }) => {
-    // the next 2 lines is verifying if user exists before trying to insert a comment in DB and breaking the code
-    const user = await this._findUserByUserId(userId);
-    if (!user) return { type: 'ERROR', message: 'User does not exists' };
-
     const insertedComment = await Comments.create({
       content,
       userId,
       date: this._formatedDate,
     });
-    return { type: null, message: insertedComment };
+    return { message: insertedComment };
   };
 
-  updateCommentById = async () => {
+  updateCommentById = async ({ content }, id) => {
+    await Comments.update(
+      { content, date: this._formatedDate },
+      { where: { id } }
+    );
+    return { message: 'Updated with success' };
+  };
 
+  deleteCommentById = async (id) => {
+    await Comments.destroy({ where: { id } });
+    return { message: 'Comment deleted with success' };
   };
 }
 
