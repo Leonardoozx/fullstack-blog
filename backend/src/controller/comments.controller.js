5a7ce810a05e7467c59f9f54803ed03a9615d174
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 01:06:33 -0300

    feat: a comments controller
    feat: implements getAllComments, postNewComments, updateCommentById,
    deleteCommentById methods for comments controller class

---
+++ backend/src/controller/comments.controller.js
@@ -0,0 +1,35 @@
+const CommentsServices = require('../comments');
+
+class CommentsController {
+  _commentsServices = new CommentsServices();
+
+  getAllComments = async (_req, res) => {
+    const { type, message } = await this._commentsServices.getAllComments();
+    if (type) return res.status(400).json({ message });
+
+    res.status(200).json(message);
+  };
+
+  postNewComment = async ({ body }, res) => {
+    const { type, message } = await this._commentsServices.postNewComment(body);
+    if (type) return res.status(400).json({ message })
+    res.status(200).json(message);
+  };
+
+  deleteCommentById = async ({ params }, res) => {
+    const { type, message } = await this._commentsServices.deleteCommentById(
+      +params.id
+    );
+    if (type) return res.status(404).json({ message });
+    res.status(204).send();
+  };
+
+  updateCommentById = async ({ params }, res) => {
+    const { type, message } = await this._commentsServices.updateCommentById(
+      +params.id
+    );
+    if (type) return res.status(404).json({ message });
+    res.status(201).json(message);
+  };
+}
+module.exports = CommentsController;
