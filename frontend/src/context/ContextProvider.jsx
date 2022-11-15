9cd53032ddfc3a29c4fdaf3ad6d9beed792e0dab
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 18:51:31 -0300

    implements userDoesExist and setUserDoesNotExist state

--- frontend/src/context/ContextProvider.jsx
+++ frontend/src/context/ContextProvider.jsx
@@ -4,32 +4,32 @@ import Context from './context';
 
 function ContextProvider({ children }) {
   const INITIAL_STATE = {
-    nameInput: '',
-    commentInput: '',
-  }
+    comment: '',
+    loginEmail: '',
+    loginPassword: '',
+  };
 
-  const [ comments, setComments ] = useState([]);
-  const [ hasNewComment, setHasNewComments ] = useState(1);
+  const [comments, setComments] = useState([]);
+  const [hasNewComment, setHasNewComments] = useState(1);
+  const [userDoesNotExist, setUserDoesNotExist] = useState(false);
   // const [ willEdit, setWillEdit ] = useState(false);
 
-  const [ genericState, setGenericState ] = useGenericState(INITIAL_STATE);
+  const [genericState, setGenericState] = useGenericState(INITIAL_STATE);
 
   const allValues = {
     genericState,
     setGenericState,
     comments,
     setHasNewComments,
+    userDoesNotExist,
+    setUserDoesNotExist,
     // willEdit,
     // setWillEdit,
     // userId,
     // setUserId
   };
 
-  return (
-    <Context.Provider value={allValues}>
-      {children}
-    </Context.Provider>
-  )
+  return <Context.Provider value={allValues}>{children}</Context.Provider>;
 }
 
 export default ContextProvider;
