2e6dee3eecd0d5952bd0a558b2bbc0ee38f09f36
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-14 23:34:17 -0300

    feat: a migration for craeting the comments table in db

---
+++ backend/src/db/migrations/2-comments.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        field: 'user_id',
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('comments');
+  },
+};
