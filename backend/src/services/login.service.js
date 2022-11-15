52e500331761d61036a0ee9620847d50911050ba
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 02:19:09 -0300

    refactor the login services for the login middleware usage

--- backend/src/services/login.service.js
+++ backend/src/services/login.service.js
@@ -1,27 +1,14 @@
 const { User } = require('../db/models');
 
 class LoginServices {
-  // async function bellow
-  _findUserByEmail = (email) => User.findOne({ where: { email } });
-
   signUp = async ({ email, name, password }) => {
-    const user = await this._findUserByEmail(email);
-    if (user) return { type: 'ERROR', message: 'User already exists' };
     const registedUser = await User.create({ email, name, password });
-    return { type: null, message: registedUser };
+    return {  message: registedUser };
   };
 
   login = async ({ email, password }) => {
-    const user = await this._findUserByEmail(email);
-    if (!user)
-      return {
-        type: 'ERROR',
-        message: 'User does not exists',
-      };
-    if (password !== user.password)
-      return { type: 'ERROR', message: 'Wrong password' };
     await User.update({ isUserLoggedIn: true }, { where: { email, password } });
-    return { type: null, message: 'Login happened with success!' };
+    return { message: 'Updated with success' }
   };
 }
 
