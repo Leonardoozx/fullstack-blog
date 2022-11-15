f4426fb2eea71c8320cb1ad91a15a014337d73be
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 01:10:37 -0300

    feat: implements the route for comment

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const loginRoutes = require('./routes/login.routes');
+const commentsRoutes = require('./routes/comments.routes');
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/login', loginRoutes);
+app.use('/comments', commentsRoutes);
 
 module.exports = app;
