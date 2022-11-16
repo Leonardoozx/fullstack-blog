38bff4376da50e59922ca1bfdf71db9c231937ea
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-16 18:46:33 -0300

    finishs form card

--- frontend/src/components/FormCard.jsx
+++ frontend/src/components/FormCard.jsx
@@ -1,24 +1,34 @@
-import { useContext } from "react";
-import context from "../context/context";
+import { useContext } from 'react';
+import context from '../context/context';
 
 function FormCard() {
-  const { genericState, setGenericState } = useContext(context);
+  const { genericState, setGenericState, username } = useContext(context);
 
   const handleSubmitClick = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmitClick} id="share-form">
-      <textarea
-        name="comment"
-        placeholder="Share something cool that happenned in your day... :)"
-        onChange={setGenericState}
-        value={genericState.comment}
-      />
-      <button type="submit">Share</button>
+    <form
+      className="form-container"
+      onSubmit={handleSubmitClick}
+      id="share-form"
+    >
+      <div id="form-info-container">
+        <div>
+          <p>{username || localStorage.getItem('username')}</p>
+          <textarea
+            name="comment"
+            placeholder="Share something cool that happenned in your day... :)"
+            onChange={setGenericState}
+            value={genericState.comment}
+            maxLength={140}
+          />
+          <button type="submit">Share</button>
+        </div>
+      </div>
     </form>
-  )
+  );
 }
 
 export default FormCard;
