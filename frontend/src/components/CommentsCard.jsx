40f97ca8c4ca84f7465b11ecda4e96829405fdfc
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-16 18:46:16 -0300

    finishs comments card

--- frontend/src/components/CommentsCard.jsx
+++ frontend/src/components/CommentsCard.jsx
@@ -1,21 +1,30 @@
 function CommentsCard(comment) {
-  console.log(comment)
-  const user = { email: '' };
+  const userId = +localStorage.getItem('userId') || '';
   return (
-    <div className="comments-container" key={comment.id}>
-      <div className="comment-info-container">
-        <p>{comment.user.name}</p>
+    <div className="comments-info-container">
+      <div className="comments-info">
+        <p className="comments-name">{comment.user.name}</p>
         <p>{comment.content}</p>
-        <p className="comment-date">{comment.date}</p>
+        <p className="comments-date">{comment.date}</p>
       </div>
-      {comment.user.email === user.email &&
-        (<div className="comment-btns-container">
-          <button type="button">edit</button>
-          <button type="button">delete</button>
-        </div>)
-      }
+      {comment.userId === userId && (
+        <div className="comment-btns-container">
+          <button type="button">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png"
+              alt="edit"
+            />
+          </button>
+          <button type="button">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1799/1799391.png"
+              alt="delete"
+            />
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default CommentsCard;
