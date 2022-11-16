4ffc4755166ce4a22afa624d27675e1b95c646f8
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-15 21:44:46 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,10 @@ const userRoutes = require('./routes/user.routes');
 const app = express();
 
 app.use(cors());
+app.use(cors());
+app.use(cors());
+app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
