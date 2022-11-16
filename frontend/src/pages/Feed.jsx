3848c918b31e0ca9a4809db3cb4639e82ba581fd
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-16 18:46:47 -0300

    finishs feed component

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,19 +1,20 @@
-import { useContext } from "react"
-import CommentsCard from "../components/CommentsCard"
-import FormCard from "../components/FormCard"
-import context from "../context/context"
+import { useContext } from 'react';
+import CommentsCard from '../components/CommentsCard';
+import FormCard from '../components/FormCard';
+import context from '../context/context';
 
 function Feed() {
   const { comments } = useContext(context);
   return (
-    <div id="feed-container">
-      {/* sidebar (lado esquerdo) */}
-      {/* componente principal no meio */}
-      {/* contagem de usuários logados (bônus se tiver tempo - lado direito) */}
+    <div className="feed-container">
       <FormCard />
-      {comments?.map((comment) => <CommentsCard {...comment} />)}
+      <div className="comments-container">
+        {comments?.map((comment) => (
+          <CommentsCard key={comment.id} {...comment} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
