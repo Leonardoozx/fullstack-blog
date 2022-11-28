be2e1e4716a7abec89e3b0dc5a71c11c85cbaf35
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-11-28 18:37:14 -0300

    rename the frontend services folder

--- frontend/src/services/loginRequests.js
+++ frontend/src/services/loginRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://fullstack-blog-backend.up.railway.app',
+  baseURL: 'http://localhost:3001',
 });
 
 export const loginPostRequest = async (email, password) => {
